649e4135a3028a2291a7bce0d5d21993152514b1
Ashley <ohhsoash@gmail.com>
2024-07-09 13:27:17 +0100

    Update count_mods.py

--- count_mods.py
+++ count_mods.py
@@ -1,18 +1,23 @@
 import json
 import os
 
-def count_mods_in_json_files(directory):
+def count_mods_in_specific_files(files):
     mod_count = 0
-    for filename in os.listdir(directory):
-        if filename.endswith('.json'):
-            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as f:
-                data = json.load(f)
-                mod_count += len(data)
+    for filepath in files:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            mod_count += len(data.get("Mods", []))
     return mod_count
 
 if __name__ == "__main__":
-    directory = './'  # Adjust to the path where your JSON files are located
-    total_mods = count_mods_in_json_files(directory)
+    files = [
+        './PS3/game-cheats.json',
+        './PS3/game-mods.json',
+        './PS3/homebrew.json',
+        './PS3/resources.json',
+        './XBOX360/plugins.json'
+    ]
+    total_mods = count_mods_in_specific_files(files)
     badge_data = {
         "schemaVersion": 1,
         "label": "mods",
